36dea3ae288e4e356f71f5659c168cffb703c59f
Parlance <parlance@vicr123.com>
2023-09-10 11:03:09 +0000

    Update Translations

---
+++ libthezzz/translations/he.ts
@@ -0,0 +1,467 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<TS version="2.1" language="he">
+  <context>
+    <name>BodyProviderEditor</name>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/providereditor/bodyprovidereditor.ui" line="14" />
+      <source>Form</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/providereditor/bodyprovidereditor.ui" line="49" />
+      <source>Body</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/providereditor/bodyprovidereditor.cpp" line="26" />
+      <source>Body</source>
+    </message>
+  </context>
+  <context>
+    <name>DescriptionProviderEditor</name>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/providereditor/descriptionprovidereditor.ui" line="14" />
+      <source>Form</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/providereditor/descriptionprovidereditor.ui" line="20" />
+      <location filename="../widgets/providereditor/descriptionprovidereditor.ui" line="42" />
+      <source>Description</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/providereditor/descriptionprovidereditor.ui" line="35" />
+      <source>Title</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/providereditor/descriptionprovidereditor.cpp" line="25" />
+      <source>Desc</source>
+    </message>
+  </context>
+  <context>
+    <name>EndpointProviderEditor</name>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/endpointprovidereditor.ui" line="14" />
+      <source>Form</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/endpointprovidereditor.ui" line="48" />
+      <source>GET</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/endpointprovidereditor.ui" line="57" />
+      <source>POST</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/endpointprovidereditor.ui" line="66" />
+      <source>PUT</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/endpointprovidereditor.ui" line="75" />
+      <source>PATCH</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/endpointprovidereditor.ui" line="84" />
+      <source>DELETE</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/endpointprovidereditor.ui" line="93" />
+      <source>OPTIONS</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/endpointprovidereditor.ui" line="102" />
+      <source>HEAD</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/endpointprovidereditor.ui" line="123" />
+      <source>Endpoint</source>
+    </message>
+  </context>
+  <context>
+    <name>EnvironmentProvider</name>
+    <message>
+      <translation></translation>
+      <location filename="../providers/environmentprovider.cpp" line="20" />
+      <location filename="../providers/environmentprovider.cpp" line="123" />
+      <source>Production</source>
+    </message>
+  </context>
+  <context>
+    <name>EnvironmentProviderEditor</name>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/providereditor/environmentprovidereditor.ui" line="14" />
+      <source>Form</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/providereditor/environmentprovidereditor.ui" line="52" />
+      <source>Environment</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/providereditor/environmentprovidereditor.ui" line="59" />
+      <source>Environment Variables allow you to set common variables and set secrets that can be used by other requests in this workspace.</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/providereditor/environmentprovidereditor.ui" line="81" />
+      <location filename="../widgets/providereditor/environmentprovidereditor.ui" line="86" />
+      <location filename="../widgets/providereditor/environmentprovidereditor.ui" line="91" />
+      <source>Var</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/providereditor/environmentprovidereditor.ui" line="96" />
+      <location filename="../widgets/providereditor/environmentprovidereditor.cpp" line="42" />
+      <source>Variable</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/providereditor/environmentprovidereditor.ui" line="101" />
+      <location filename="../widgets/providereditor/environmentprovidereditor.cpp" line="43" />
+      <source>Secret?</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/providereditor/environmentprovidereditor.ui" line="106" />
+      <source>Production</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/providereditor/environmentprovidereditor.ui" line="111" />
+      <source>Staging</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/providereditor/environmentprovidereditor.ui" line="116" />
+      <source>baseUrl</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/providereditor/environmentprovidereditor.ui" line="138" />
+      <source>envthing</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/providereditor/environmentprovidereditor.ui" line="143" />
+      <source>envthing2</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/providereditor/environmentprovidereditor.ui" line="148" />
+      <source>token</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/providereditor/environmentprovidereditor.ui" line="170" />
+      <source>password</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/providereditor/environmentprovidereditor.ui" line="175" />
+      <source>password2</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/providereditor/environmentprovidereditor.ui" line="180" />
+      <location filename="../widgets/providereditor/environmentprovidereditor.cpp" line="86" />
+      <location filename="../widgets/providereditor/environmentprovidereditor.cpp" line="106" />
+      <source>New Variable...</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/providereditor/environmentprovidereditor.ui" line="222" />
+      <source>Actions</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/providereditor/environmentprovidereditor.ui" line="229" />
+      <source>Create New Environment</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/providereditor/environmentprovidereditor.cpp" line="30" />
+      <source>Env</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/providereditor/environmentprovidereditor.cpp" line="133" />
+      <source>For Environment %1</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/providereditor/environmentprovidereditor.cpp" line="134" />
+      <location filename="../widgets/providereditor/environmentprovidereditor.cpp" line="149" />
+      <location filename="../widgets/providereditor/environmentprovidereditor.cpp" line="153" />
+      <source>Delete Environment</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/providereditor/environmentprovidereditor.cpp" line="140" />
+      <source>Unable to delete environment</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/providereditor/environmentprovidereditor.cpp" line="141" />
+      <source>Every workspace must have at least one environment.</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/providereditor/environmentprovidereditor.cpp" line="142" />
+      <source>To delete the %1 environment, create a new environment first.</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/providereditor/environmentprovidereditor.cpp" line="150" />
+      <source>Do you want to delete the environment %1?</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/providereditor/environmentprovidereditor.cpp" line="151" />
+      <source>Deleting the environment will also delete all environment variables associated with it.</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/providereditor/environmentprovidereditor.cpp" line="170" />
+      <source>New Environment</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/providereditor/environmentprovidereditor.cpp" line="170" />
+      <source>What do you want to call the new environment?</source>
+    </message>
+  </context>
+  <context>
+    <name>HeaderProviderEditor</name>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/providereditor/headerprovidereditor.ui" line="14" />
+      <source>Form</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/providereditor/headerprovidereditor.ui" line="52" />
+      <source>Headers</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/providereditor/headerprovidereditor.ui" line="74" />
+      <source>Header</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/providereditor/headerprovidereditor.ui" line="79" />
+      <source>Value</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/providereditor/headerprovidereditor.cpp" line="26" />
+      <source>Hdrs</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/providereditor/headerprovidereditor.cpp" line="95" />
+      <location filename="../widgets/providereditor/headerprovidereditor.cpp" line="105" />
+      <source>New Header...</source>
+    </message>
+  </context>
+  <context>
+    <name>ReplyBodyWidget</name>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/replyinspector/replybodywidget.ui" line="14" />
+      <source>Form</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/replyinspector/replybodywidget.ui" line="37" />
+      <source>Raw Body</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/replyinspector/replybodywidget.cpp" line="25" />
+      <source>Raw</source>
+    </message>
+  </context>
+  <context>
+    <name>ReplyHeadersWidget</name>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/replyinspector/replyheaderswidget.ui" line="14" />
+      <source>Form</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/replyinspector/replyheaderswidget.ui" line="52" />
+      <source>Headers</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/replyinspector/replyheaderswidget.ui" line="74" />
+      <source>Header</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/replyinspector/replyheaderswidget.ui" line="79" />
+      <source>Value</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/replyinspector/replyheaderswidget.cpp" line="18" />
+      <source>Hdr</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/replyinspector/replyheaderswidget.cpp" line="37" />
+      <source>For header %1</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/replyinspector/replyheaderswidget.cpp" line="38" />
+      <source>Copy Header</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/replyinspector/replyheaderswidget.cpp" line="41" />
+      <source>Copy Value</source>
+    </message>
+  </context>
+  <context>
+    <name>WorkspaceFile</name>
+    <message>
+      <translation></translation>
+      <location filename="../workspacefile.cpp" line="87" />
+      <source>Untitled Workspace</source>
+    </message>
+  </context>
+  <context>
+    <name>ZzzReplyInspector</name>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/zzzreplyinspector.ui" line="14" />
+      <source>Form</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/zzzreplyinspector.ui" line="94" />
+      <source>Unable to send request</source>
+    </message>
+    <message numerus="yes">
+      <translation>
+        <numerusform></numerusform>
+        <numerusform></numerusform>
+        <numerusform></numerusform>
+      </translation>
+      <location filename="../widgets/zzzreplyinspector.cpp" line="39" />
+      <source>in %n msec</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/zzzreplyinspector.cpp" line="45" />
+      <source>Fail</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/zzzreplyinspector.cpp" line="84" />
+      <source>Processing...</source>
+    </message>
+  </context>
+  <context>
+    <name>ZzzReplyViewer</name>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/zzzreplyviewer.ui" line="14" />
+      <source>Form</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/zzzreplyviewer.ui" line="35" />
+      <source>Responses</source>
+    </message>
+  </context>
+  <context>
+    <name>ZzzRequest</name>
+    <message>
+      <translation></translation>
+      <location filename="../zzzrequest.cpp" line="16" />
+      <location filename="../zzzrequest.cpp" line="20" />
+      <source>Untitled Request</source>
+    </message>
+  </context>
+  <context>
+    <name>ZzzRequestEditor</name>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/zzzrequesteditor.ui" line="14" />
+      <source>Form</source>
+    </message>
+  </context>
+  <context>
+    <name>ZzzRequestFolder</name>
+    <message>
+      <translation></translation>
+      <location filename="../zzzrequestfolder.cpp" line="14" />
+      <location filename="../zzzrequestfolder.cpp" line="18" />
+      <source>Untitled Request Folder</source>
+    </message>
+  </context>
+  <context>
+    <name>ZzzWorkspaceEditor</name>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/zzzworkspaceeditor.ui" line="14" />
+      <source>Form</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/zzzworkspaceeditor.ui" line="77" />
+      <location filename="../widgets/zzzworkspaceeditor.cpp" line="270" />
+      <source>New Request</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/zzzworkspaceeditor.cpp" line="151" />
+      <location filename="../widgets/zzzworkspaceeditor.cpp" line="155" />
+      <source>Delete Request Folder</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/zzzworkspaceeditor.cpp" line="152" />
+      <source>Do you want to delete the request folder?</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/zzzworkspaceeditor.cpp" line="153" />
+      <source>Deleting the request folder will also delete all the requests contained within.</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/zzzworkspaceeditor.cpp" line="267" />
+      <source>For %1</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/zzzworkspaceeditor.cpp" line="273" />
+      <source>New Request Folder</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/zzzworkspaceeditor.cpp" line="279" />
+      <source>Delete</source>
+    </message>
+  </context>
+</TS>
